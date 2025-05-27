15a0b5f055f54e187bd68811433dfeb886081e44
BRAINCaen <brain.caen@gmail.com>
2025-05-27 22:05:09 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // Configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyD7uBuAQaOhZ02owkZEuMKC5Vji6PrB2f8",
-  authDomain: "synergia-app-f27e7.firebaseapp.com",
-  projectId: "synergia-app-f27e7",
-  storageBucket: "synergia-app-f27e7.firebasestorage.app",
-  messagingSenderId: "201912738922",
-  appId: "1:201912738922:web:96a9f0a6ffadf9cf899613",
-  measurementId: "G-S5WPV574KJ"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyD7uBuAQaOhZ02owkZEuMKC5Vji6PrB2f8",
+    authDomain: "synergia-app-f27e7.firebaseapp.com",
+    projectId: "synergia-app-f27e7",
+    storageBucket: "synergia-app-f27e7.firebasestorage.app",
+    messagingSenderId: "201912738922",
+    appId: "1:201912738922:web:2fcc1e49293bb632899613",
+    measurementId: "G-EGJ79SCMWX"
+  };
 
 // Initialisation de firebase (seulement si elle n'est pas déjà initialisée)
 let firebaseApp;
